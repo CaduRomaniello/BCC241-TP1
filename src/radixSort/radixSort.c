266b5171fba22ecf3aa57e2291093b4d31a6340d
CaduRomaniello <cadu_romaniello@hotmail.com>
2022-04-18 18:05:54 -0300

    .

--- src/radixSort/radixSort.c
+++ src/radixSort/radixSort.c
@@ -1,30 +1,30 @@
 #include "radixSort.h"
 
-countingSort(int[] content) {
+void countingSort(int* content, int length) {
 
-        int maximum = _findMaximum(content);
-        int[] countingArray = new int[maximum + 1];
-        int[] sortedArray = new int[content.length + 1];
-        Arrays.fill(countingArray, 0);
+    int maximum = _findMaximum(content);
+    int* countingArray = calloc(maximum + 1, sizeof(int));
+    int* sortedArray = malloc(length + 1 * sizeof(int));
 
-        for (int i : content) {
-            countingArray[i] += 1;
-        }
-
-        for (int i = 1; i <= maximum; i++) {
-            countingArray[i] += countingArray[i - 1];
-        }
-
-        for (int i = maximum - 1; i >= 0; i--) {
-            int value = content[i];
-            int pos = countingArray[value];
-            sortedArray[pos - 1] = value;
-            countingArray[value] -= 1;
-        }
-
-        // Copy the sorted elements into original array
-        for (int i = 0; i < content.length; i++) {
-            content[i] = sortedArray[i];
-        }
-
+    for (int i = 0; i < length; i++) {
+        countingArray[i] += 1;
     }
+
+    for (int i = 1; i <= maximum; i++) {
+        countingArray[i] += countingArray[i - 1];
+    }
+
+    for (int i = maximum - 1; i >= 0; i--) {
+        int value = content[i];
+        int pos = countingArray[value];
+
+        sortedArray[pos - 1] = value;
+        countingArray[value] -= 1;
+    }
+
+    // Copy the sorted elements into original array
+    for (int i = 0; i < length; i++) {
+        content[i] = sortedArray[i];
+    }
+
+}
