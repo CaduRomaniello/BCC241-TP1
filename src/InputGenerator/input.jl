c5842380fd49b5d48435508c29280f76374670fc
CaduRomaniello <cadu_romaniello@hotmail.com>
2022-04-21 14:54:55 -0300

    main refeito

--- src/InputGenerator/input.jl
+++ src/InputGenerator/input.jl
@@ -7,21 +7,20 @@ function main(ARGS)
     local size = parse(Int64, ARGS[1])
 
     for i = 1:20
-        mkpath("../../data/input/n$(size)/")
-        local file = open("../../data/input/n$(size)/instance-n$(size)-$(i).in", "w")
-        write(file, "$(size)\n")
+        mkpath("$(@__DIR__)/../../data/input/n$(size)/")
+        local file = open("$(@__DIR__)/../../data/input/n$(size)/instance-n$(size)-$(i).in", "w")
 
         local instance = zeros(Int64, size)
         for j = 1:size
             local randomNumber = 0
 
-            while true
+            # while true
                 randomNumber = rand(1:size)
 
-                if !(randomNumber in instance)
-                    break
-                end
-            end
+                # if !(randomNumber in instance)
+                #     break
+                # end
+            # end
 
             instance[j] = randomNumber
             write(file, "$(randomNumber)\n")
--- src/main.c
+++ src/main.c
@@ -1,32 +1,79 @@
 #include "insertionSort/insertionSort.h"
 #include "mergeSort/mergeSort.h"
 #include "radixSort/radixSort.h"
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
 
-#define TAM 100000000
+int main(int argc, char** argv){
 
-int main(){
-    int* vet = (int*) malloc(sizeof(int) * TAM);;
-    int count = 1;
+    int* valores;
+    double tempo[20];
+    FILE* instancia;
+    char tempString[100];
 
-    for (int i = TAM-1; i >= 0; i--){
-        vet[i] = count++;
+    time_t start, end;
+
+    int TAM = atoi(argv[1]);
+    char algoritmo[14];
+    strcpy(algoritmo, argv[2]);
+
+    bool isMerge = (strcmp("MergeSort", algoritmo) == 0) ? true : false;
+    bool isInsertion = (strcmp("InsertionSort", algoritmo) == 0) ? true : false;
+    bool isRadix = (strcmp("RadixSort", algoritmo) == 0) ? true : false;
+
+    if(!(isMerge || isInsertion || isRadix)){
+        printf("Algoritmo selecionado invalido. Os algoritmos sao:\n");
+        printf("- MergeSort\n");
+        printf("- InsertionSort\n");
+        printf("- RadixSort\n");
+        printf("Algoritmo selecionado: %s\n", algoritmo);
+
+        exit(0);
     }
 
-    printf("Antes:\n");
-    // for (int i = 0; i < TAM; i++){
-    //     printf("%d ", vet[i]);
-    // }
-    // printf("\n");
+    printf("%s\n\n", algoritmo);
 
-    printf("RadixSort\n");
-    mergeSort(vet, TAM);
+    
+    valores = (int*) malloc(TAM * sizeof(int));
+    
+    for (int i = 1; i <= 20; i++){
+        sprintf(tempString, "../data/input/n%d/instance-n%d-%d.in", TAM, TAM, i);
+        instancia = fopen(tempString, "r");
 
-    printf("Depois:\n");
-    // for (int i = 0; i < TAM; i++){
-    //     printf("%d ", vet[i]);
-    // }
-    // printf("\n");
+        for (int j = 0; j < TAM; j++) {
+            fscanf(instancia, "%d", &valores[j]);
+        }
 
-    free(vet);
+        // setar o tempo inicial
+        if (isMerge) {
+            start = clock();
+            mergeSort(valores, TAM);
+            end = clock();
+
+        } else if (isInsertion) {
+            start = clock();
+            insertionSort(valores, TAM);
+            end = clock();
+
+        } else if (isRadix) {
+            start = clock();
+            radixSort(valores, TAM);
+            end = clock();
+        }
+
+        tempo[i] = difftime(end, start) / CLOCKS_PER_SEC;
+
+        fclose(instancia);
+    }
+
+    double media = 0.0;
+    for(int i = 1; i <= 20; i++){
+        printf("Instancia %d: %lf\n", i, tempo[i-1]);
+        media += tempo[i-1];
+    }
+    printf("\nTempo medio: %lf\n", media / 20.0);
+
+    free(valores);
     return 0;
 }
